a1aee539dd4e088ec60733adc85e10d12b19e533
Brad Fitzpatrick <brad@danga.com>
2003-10-05 21:48:05 +0000

    Java API by Richard Russo
    git-svn-id: http://code.sixapart.com/svn/memcached@143 b0b603af-a30f-0410-a34e-baf09ae79d0b

---
+++ trunk/api/java/com/danga/MemCached/SockIO.java
@@ -0,0 +1,59 @@
+/**
+ * MemCached Java client, utility class for Socket IO
+ * Copyright (c) 2003
+ * Richard 'toast' Russo <russor@msoe.edu>
+ * http://people.msoe.edu/~russor/memcached
+ *
+ *
+ * This module is Copyright (c) 2003 Richard Russo.
+ * All rights reserved.
+ * You may distribute under the terms of the GNU General Public License
+ * This is free software. IT COMES WITHOUT WARRANTY OF ANY KIND.
+ *
+ * @author  Richard 'toast' Russo <russor@msoe.edu>
+ * @version 0.9.0
+ */
+
+
+package com.danga.MemCached; 
+
+
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+
+
+class SockIO {
+    Socket sock;
+    DataInputStream in;
+    DataOutputStream out;
+    boolean closed = false;
+    
+    public SockIO(String host, int port) throws IOException {
+        sock = new Socket(host,port);
+        in = new DataInputStream(sock.getInputStream());
+        out = new DataOutputStream(sock.getOutputStream());
+        
+    }
+    
+    public void close() {
+        closed = true;
+        try {
+        in.close();
+        out.close();
+        sock.close();
+        } catch (IOException e) {
+        }
+        
+    }
+    public DataInputStream in() {
+        return in;
+    }
+    public DataOutputStream out() {
+        return out;
+    }
+    public boolean isConnected() {
+        return (closed && sock.isConnected());
+    }
+}
