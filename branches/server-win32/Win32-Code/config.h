69511a9aff2d721546545eb0ca16433d60dbdba4
Germán Méndez Bravo <kronuz@users.sf.net>
2006-04-28 21:48:45 +0000

    Win32 support. Version updated to 1.1.13.
    git-svn-id: http://code.sixapart.com/svn/memcached@266 b0b603af-a30f-0410-a34e-baf09ae79d0b

--- branches/server-win32/Win32-Code/config.h
+++ branches/server-win32/Win32-Code/config.h
@@ -8,19 +8,19 @@
 #define PACKAGE_NAME "memcached Server"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "memcached 1.1.12"
+#define PACKAGE_STRING "memcached 1.1.13"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_DESCRIPTION "memcached 1.1.12 is a high-performance, distributed memory object caching system, generic in nature, but intended for use in speeding up dynamic web applications by alleviating database load. Win32 port by Kronuz."
+#define PACKAGE_DESCRIPTION "memcached 1.1.13 is a high-performance, distributed memory object caching system, generic in nature, but intended for use in speeding up dynamic web applications by alleviating database load. Win32 port by Kronuz."
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "memcached"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.1.12"
+#define PACKAGE_VERSION "1.1.13"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.1.12"
+#define VERSION "1.1.13"
