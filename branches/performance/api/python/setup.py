4d27d29a65eeb2650fe0c4d52252d901f8abe8bb
Steven Grimm <sgrimm@facebook.com>
2006-10-15 08:08:25 +0000

    New branch for experimental performance-related changes.
    git-svn-id: http://code.sixapart.com/svn/memcached@414 b0b603af-a30f-0410-a34e-baf09ae79d0b

---
+++ branches/performance/api/python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import memcache
+
+setup(name="python-memcached",
+      version=memcache.__version__,
+      author="Evan Martin",
+      author_email="martine@danga.com",
+      url="http://www.danga.com/memcached/",
+      py_modules=["memcache"])
+
